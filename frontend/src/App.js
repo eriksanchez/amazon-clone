af5535e30d0daf5e4f50a72b985ead1821a0c742
eriksanchez <erikcsanchezdev@gmail.com>
2021-06-21 22:39:15 -0500

    frontend additions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import data from "./data"
-
+import Product from "./components/Product"
 function App() {
   return (
     <div className="grid-container">
             <header className="row">
                 <div>
-                    <a className="brand" href="/">Amazon</a>
+                    <a className="brand" href="/">Amazon by Erik</a>
                 </div>
                 <div>
                     <a href="/cart">Cart</a>
@@ -18,28 +18,8 @@ function App() {
 
                {
                  data.products.map(product =>(
-                  <div key={product._id} className="card">
-                  <a href={`/product/${product._id}`}>
-                      
-                      <img className="medium" src={product.image} alt={product.name}/>
-                  </a>
-                  <div className="card-body">
-                      <a href={`/product/${product._id}`}>
-                          <h2>{product.name}</h2>
-                      </a>
-                      <div className="rating">
-                          <span><i className="fa fa-star"></i></span>
-                          <span><i className="fa fa-star"></i></span>
-                       <span><i className="fa fa-star"></i></span>
-                       <span><i className="fa fa-star"></i></span>
-                       <span><i className="fa fa-star-o"></i></span>
-                      </div>
-                      <div className="price">
-                          ${product.price}
-                      </div>
-                  </div>
-              </div>
                   
+                  <Product key={product._id} product={product}></Product>
                  ))
                }
                    
