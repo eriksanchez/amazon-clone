da46e9f8233b253cd3e3c611f9004a36976313b8
eriksanchez <erikcsanchezdev@gmail.com>
2021-06-21 22:13:45 -0500

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="grid-container">
             <header className="row">
                 <div>
-                    <a className="brand" href="index.html">Amazon</a>
+                    <a className="brand" href="/">Amazon</a>
                 </div>
                 <div>
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign In</a>
                 </div>
             </header>
             <main>
