cb3f2998d09d8f0e3639e3b0a343ced72423c511
eriksanchez <erikcsanchezdev@gmail.com>
2021-06-29 12:49:04 -0500

    created register screen

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,24 +1,30 @@
 import React,{useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
-import { signin } from '../actions/userActions';
+import { register } from '../actions/userActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
-function SigninScreen(props) {
-
+function RegisterScreen(props) {
+    const [name, setName] = useState('')
     const[email,setEmail] = useState('');
     const[password, setPassword] = useState('')
+    const[confirmPassword, setConfirmPassword] = useState('')
     
     const redirect = props.location.search? props.location.search.split('=')[1]: '/';
 
-    const userSignin = useSelector((state) => state.userSignin);
-    const {userInfo, loading, error} = userSignin;
+    const userRegister = useSelector((state) => state.userRegister);
+    const {userInfo, loading, error} = userRegister;
 
     const dispatch = useDispatch()
     const submitHandler = (e) =>{
         e.preventDefault();
-        dispatch(signin(email, password))
+        if(password !== confirmPassword){
+            alert('Password and confirm password are not matching')
+        }else{
+            dispatch(register(name, email, password))
+        }
+        
         
     };
     useEffect(() =>{
@@ -31,10 +37,14 @@ function SigninScreen(props) {
         <div>
             <form className="form" onSubmit={submitHandler}>
                 <div>
-                    <h1> Sign In</h1>
+                    <h1> Create Account</h1>
                 </div>
                 {loading && <LoadingBox></LoadingBox>}
                 {error && <MessageBox variant="danger">{error}</MessageBox>}
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" placeholder="Enter name" required onChange={e => setName(e.target.value)}></input>
+                </div>
                 <div>
                     <label htmlFor="email">Email address</label>
                     <input type="email" id="email" placeholder="Enter email" required onChange={e => setEmail(e.target.value)}></input>
@@ -44,13 +54,17 @@ function SigninScreen(props) {
                     <input type="password" id="password" placeholder="Enter password" required onChange={e => setPassword(e.target.value)}></input>
                 </div>
                 <div>
+                    <label htmlFor="confirmPassword">Comfirm Password</label>
+                    <input type="password" id="confirmPassword" placeholder="Enter confirm password" required onChange={e => setConfirmPassword(e.target.value)}></input>
+                </div>
+                <div>
                     <label/>
-                    <button className="primary" type="submit">Sign In</button>
+                    <button className="primary" type="submit">Register</button>
                 </div>
                 <div> <label/>
                     <div>
-                        New customer {' '}
-                        <Link to={`/register?redirect=${redirect}`}>Create your account</Link>
+                        Already have an account? {' '}
+                        <Link to={`/signin?redirect=${redirect}`}>Sign-In</Link>
                     </div>
                 </div>
             </form>
@@ -58,4 +72,4 @@ function SigninScreen(props) {
     )
 }
 
-export default SigninScreen
+export default RegisterScreen
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import {createStore, compose, applyMiddleware, combineReducers} from 'redux'
 import thunk from "redux-thunk"
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducer';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducer';
 
 const initialState = {
     userSignin: {
@@ -16,7 +16,8 @@ const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-    userSignin: userSigninReducer
+    userSignin: userSigninReducer, 
+    userRegister: userRegisterReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
