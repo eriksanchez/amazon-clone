11a3b80fa469633e8b550128688e13f5a6f05c72
eriksanchez <erikcsanchezdev@gmail.com>
2021-07-01 07:11:54 -0500

    created order screen...

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -25,4 +25,13 @@ orderRouter.post('/', isAuth, expressAsyncHandler(async(req,res) => {
     }
 }))
 
+orderRouter.get('/:id', isAuth, expressAsyncHandler(async(req,res) =>{
+    const order = await Order.findById(req.params.id);
+    if(order){
+        res.send(order);
+    }else{
+        res.status(400).send({message: 'Order Not Found'})
+    }
+}))
+
 export default orderRouter
